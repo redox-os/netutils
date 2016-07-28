3460de6426e2b1a41d5613bc9f98fe872e485b93
Jeremy Soller <jackpot51@gmail.com>
2016-07-28 16:50:50 -0600

    Background mode

--- src/dhcpd/main.rs
+++ src/dhcpd/main.rs
@@ -1,12 +1,12 @@
+use std::{env, thread, time};
 use std::fs::File;
 use std::io::{Read, Write};
-use std::time;
 
 use dhcp::Dhcp;
 
 mod dhcp;
 
-fn main(){
+fn dhcp(quiet: bool) {
     let mut current_mac = [0; 6];
     File::open("netcfg:mac").unwrap().read(&mut current_mac).unwrap();
 
@@ -14,7 +14,9 @@ fn main(){
         let mut current_ip = [0; 4];
         File::open("netcfg:ip").unwrap().read(&mut current_ip).unwrap();
 
-        println!("DHCP: Current IP: {:?}", current_ip);
+        if ! quiet {
+            println!("DHCP: Current IP: {:?}", current_ip);
+        }
     }
 
     let tid = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().subsec_nanos();
@@ -50,13 +52,17 @@ fn main(){
 
         let _sent = socket.write(discover_data).unwrap();
 
-        println!("DHCP: Sent Discover");
+        if ! quiet {
+            println!("DHCP: Sent Discover");
+        }
     }
 
     let mut offer_data = [0; 65536];
     socket.read(&mut offer_data).unwrap();
     let offer = unsafe { &* (offer_data.as_ptr() as *const Dhcp) };
-    println!("DHCP: Offer IP: {:?}, Server IP: {:?}", offer.yiaddr, offer.siaddr);
+    if ! quiet {
+        println!("DHCP: Offer IP: {:?}, Server IP: {:?}", offer.yiaddr, offer.siaddr);
+    }
 
     {
         let mut subnet_option = None;
@@ -76,27 +82,49 @@ fn main(){
                         }
                         match *option {
                             1 => {
-                                println!("DHCP: Subnet Mask: {:?}", data);
+                                if ! quiet {
+                                    println!("DHCP: Subnet Mask: {:?}", data);
+                                }
                                 if data.len() == 4 && subnet_option.is_none() {
                                     subnet_option = Some(Vec::from(data));
                                 }
                             },
                             3 => {
-                                println!("DHCP: Router: {:?}", data);
+                                if ! quiet {
+                                    println!("DHCP: Router: {:?}", data);
+                                }
                                 if data.len() == 4  && router_option.is_none() {
                                     router_option = Some(Vec::from(data));
                                 }
                             },
                             6 => {
-                                println!("DHCP: Domain Name Server: {:?}", data);
+                                if ! quiet {
+                                    println!("DHCP: Domain Name Server: {:?}", data);
+                                }
                                 if data.len() == 4 && dns_option.is_none() {
                                     dns_option = Some(Vec::from(data));
                                 }
                             },
-                            51 => println!("DHCP: Lease Time: {:?}", data),
-                            53 => println!("DHCP: Message Type: {:?}", data),
-                            54 => println!("DHCP: Server ID: {:?}", data),
-                            _ => println!("DHCP: {}: {:?}", option, data)
+                            51 => {
+                                if ! quiet {
+                                    println!("DHCP: Lease Time: {:?}", data);
+                                }
+                            },
+                            53 => {
+                                if ! quiet {
+                                    println!("DHCP: Message Type: {:?}", data);
+                                }
+                            },
+                            54 => {
+                                if ! quiet {
+                                    println!("DHCP: Server ID: {:?}", data);
+                                }
+                            },
+                            _ => {
+                                if ! quiet {
+                                    println!("DHCP: {}: {:?}", option, data);
+                                }
+                            }
                         }
                     }
                 },
@@ -109,7 +137,9 @@ fn main(){
             let mut new_ip = [0; 4];
             File::open("netcfg:ip").unwrap().read(&mut new_ip).unwrap();
 
-            println!("DHCP: New IP: {:?}", new_ip);
+            if ! quiet {
+                println!("DHCP: New IP: {:?}", new_ip);
+            }
         }
 
         if let Some(subnet) = subnet_option {
@@ -118,7 +148,9 @@ fn main(){
             let mut new_subnet = [0; 4];
             File::open("netcfg:ip_subnet").unwrap().read(&mut new_subnet).unwrap();
 
-            println!("DHCP: New Subnet: {:?}", new_subnet);
+            if ! quiet {
+                println!("DHCP: New Subnet: {:?}", new_subnet);
+            }
         }
 
         if let Some(router) = router_option {
@@ -127,7 +159,9 @@ fn main(){
             let mut new_router = [0; 4];
             File::open("netcfg:ip_router").unwrap().read(&mut new_router).unwrap();
 
-            println!("DHCP: New Router: {:?}", new_router);
+            if ! quiet {
+                println!("DHCP: New Router: {:?}", new_router);
+            }
         }
 
         if let Some(dns) = dns_option {
@@ -136,7 +170,9 @@ fn main(){
             let mut new_dns = [0; 4];
             File::open("netcfg:dns").unwrap().read(&mut new_dns).unwrap();
 
-            println!("DHCP: New DNS: {:?}", new_dns);
+            if ! quiet {
+                println!("DHCP: New DNS: {:?}", new_dns);
+            }
         }
     }
 
@@ -169,13 +205,37 @@ fn main(){
 
         let _sent = socket.write(request_data).unwrap();
 
-        println!("DHCP: Sent Request");
+        if ! quiet {
+            println!("DHCP: Sent Request");
+        }
     }
 
     {
         let mut ack_data = [0; 65536];
         socket.read(&mut ack_data).unwrap();
         let ack = unsafe { &* (ack_data.as_ptr() as *const Dhcp) };
-        println!("DHCP: Ack IP: {:?}, Server IP: {:?}", ack.yiaddr, ack.siaddr);
+        if ! quiet {
+            println!("DHCP: Ack IP: {:?}, Server IP: {:?}", ack.yiaddr, ack.siaddr);
+        }
+    }
+}
+
+fn main(){
+    let mut background = false;
+    let mut quiet = false;
+    for arg in env::args().skip(1) {
+        match arg.as_ref() {
+            "-b" => background = true,
+            "-q" => quiet = true,
+            _ => ()
+        }
+    }
+
+    if background {
+        thread::spawn(move || {
+            dhcp(quiet);
+        });
+    } else {
+        dhcp(quiet);
     }
 }
