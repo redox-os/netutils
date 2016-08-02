9bff20da744b3cef54cdd055b30b16a06ac614b2
Sehny <sehnoutka.martin@gmail.com>
2016-08-02 21:27:24 +0200

    Reimplement nc using tcpstream.

--- src/nc/main.rs
+++ src/nc/main.rs
@@ -1,58 +1,95 @@
 use std::env;
-use std::io::{stdin, Read, Write, Result};
+use std::io::{self, stdin, Read, Write, Result};
+use std::net::TcpStream;
 use std::str;
-use std::sync::Arc;
 use std::thread;
 
-use std::cell::UnsafeCell;
-use std::fs::File;
+static MAN_PAGE: &'static str = /* @MANSTART{tail} */ r#"
+NAME
+    nc - Concatenate and redirect sockets
+SYNOPSIS
+    nc [[-h | --help] | [[-u | --udp]] [hostname:port]
+DESCRIPTION
+    Netcat (nc) is command line utility which can read and write data across network. Currently
+    it only works with IPv4 and does not support any encryption.
+OPTIONS
+    -h
+    --help
+        Print this manual page.
+    -u
+    --udp
+        Use UDP instead of default TCP. Not implemented yet.
 
-/// Redox domain socket
-pub struct Socket {
-    file: UnsafeCell<File>
+    -l
+    --listen
+        Listen for incoming connections. Not implemented yet.
+AUTHOR
+    Written by Sehny.
+"#; /* @MANEND */
+
+const BUFFER_SIZE: usize = 65636;
+
+enum TransportProtocol {
+    Tcp,
+    Udp,
 }
 
-unsafe impl Send for Socket {}
-unsafe impl Sync for Socket {}
+// enum NcMode {
+//     Connect,
+//     Listen,
+// }
 
-impl Socket {
-    pub fn open(path: &str) -> Result<Socket> {
-        let file = try!(File::open(path));
-        Ok(Socket {
-            file: UnsafeCell::new(file)
-        })
-    }
+fn connect_tcp(host: String) -> Result<()> {
+    let mut stream_read = TcpStream::connect(host.as_str()).unwrap();
+    let mut stream_write = stream_read.try_clone().unwrap();
 
-    pub fn receive(&self, buf: &mut [u8]) -> Result<usize> {
-        unsafe { (*self.file.get()).read(buf) }
-    }
+    thread::spawn(move || {
+        loop {
+            let mut buffer = [0u8; BUFFER_SIZE];
+            let count  = stream_read.read(&mut buffer).unwrap();
+            print!("{}", unsafe { str::from_utf8_unchecked(&buffer[..count]) });
+        }
+    });
 
-    pub fn send(&self, buf: &[u8]) -> Result<usize> {
-        unsafe { (*self.file.get()).write(buf) }
+    loop {
+        let mut buffer = [0; BUFFER_SIZE];
+        let count = stdin().read(&mut buffer).unwrap();
+        let _ = stream_write.write(&buffer[..count]).unwrap();
     }
 }
-
 
 fn main() {
+
     let mut args = env::args().skip(1);
-    if let Some(path) = args.next() {
-        let socket_write = Arc::new(Socket::open(&path).unwrap());
-        let socket_read = socket_write.clone();
+    let mut hostname = "".to_string();
+    let mut proto = TransportProtocol::Tcp;
+    let mut stdout = io::stdout();
 
-        println!("USING {}", path);
-
-        thread::spawn(move || {
-            loop {
-                let mut buffer = [0; 65536];
-                let count = socket_read.receive(&mut buffer).unwrap();
-                print!("{}", unsafe { str::from_utf8_unchecked(&buffer[..count]) });
+    while let Some(arg) = args.next() {
+        if arg.starts_with('-') {
+            match arg.as_str() {
+                "-h" | "--help" => {
+                    stdout.write_all(MAN_PAGE.as_bytes()).unwrap();
+                    return;
+                }
+                "-u" | "--udp" => proto = TransportProtocol::Udp,
+                "-l" | "--listen" => {
+                    println!("This functionality has not been implemented yet.");
+                    return;
+                }
+                _ => {
+                    println!("Invalid argument!");
+                    return;
+                }
             }
-        });
-
-        loop {
-            let mut buffer = [0; 65536];
-            let count = stdin().read(&mut buffer).unwrap();
-            socket_write.send(&buffer[..count]).unwrap();
+        } else {
+            hostname = arg;
         }
+    }
+
+    println!("Remote host: {}", hostname);
+    match proto {
+        TransportProtocol::Tcp => connect_tcp(hostname).unwrap(),
+        TransportProtocol::Udp => println!("Not implemented. udp"),
     }
 }
