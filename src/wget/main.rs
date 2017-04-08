ca1ef1149741dd10a3d4d8f7c8ff523d1663ac83
Jeremy Soller <jackpot51@gmail.com>
2017-04-07 20:27:45 -0600

    Better wget errors

--- src/wget/main.rs
+++ src/wget/main.rs
@@ -10,12 +10,30 @@ use hyper::net::HttpsConnector;
 fn main() {
     if let Some(url) = env::args().nth(1) {
         let client = Client::with_connector(HttpsConnector::new(hyper_rustls::TlsClient::new()));
-        let mut res = client.get(&url).send().unwrap();
-        let mut data = Vec::new();
-        res.read_to_end(&mut data).unwrap();
-        stdout().write(&data).unwrap();
+        match client.get(&url).send() {
+            Ok(mut res) => {
+                let mut data = Vec::new();
+                match res.read_to_end(&mut data) {
+                    Ok(_) => match stdout().write(&data) {
+                        Ok(_) => (),
+                        Err(err) => {
+                            writeln!(stderr(), "wget: failed to write output: {}", err).unwrap();
+                            process::exit(1);
+                        }
+                    },
+                    Err(err) => {
+                        writeln!(stderr(), "wget: failed to read response: {}", err).unwrap();
+                        process::exit(1);
+                    }
+                }
+            },
+            Err(err) => {
+                writeln!(stderr(), "wget: failed to send request: {}", err).unwrap();
+                process::exit(1);
+            }
+        }
     } else {
-        write!(stderr(), "wget: http://host:port/path\n").unwrap();
+        writeln!(stderr(), "wget http://host:port/path").unwrap();
         process::exit(1);
     }
 }
