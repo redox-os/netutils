f97aeea5cfd83d4fafc7673baae458433b0e7fd0
Jeremy Soller <jackpot51@gmail.com>
2017-04-11 21:59:41 -0600

    Add optional output file to wget Fail wget when return code is not success

--- src/wget/main.rs
+++ src/wget/main.rs
@@ -2,41 +2,61 @@ extern crate hyper;
 extern crate hyper_rustls;
 
 use std::env;
-use std::io::{stderr, stdout, Read, Write};
+use std::fs::File;
+use std::io::{self, stderr, stdout, Write};
 use std::process;
 use std::time::Duration;
 use hyper::Client;
 use hyper::net::HttpsConnector;
+use hyper::status::StatusCode;
 
-fn main() {
-    if let Some(url) = env::args().nth(1) {
-        let mut client = Client::with_connector(HttpsConnector::new(hyper_rustls::TlsClient::new()));
-        client.set_read_timeout(Some(Duration::new(5, 0)));
-        client.set_write_timeout(Some(Duration::new(5, 0)));
-        match client.get(&url).send() {
-            Ok(mut res) => {
-                let mut data = Vec::new();
-                match res.read_to_end(&mut data) {
-                    Ok(_) => match stdout().write(&data) {
-                        Ok(_) => (),
-                        Err(err) => {
-                            writeln!(stderr(), "wget: failed to write output: {}", err).unwrap();
-                            process::exit(1);
-                        }
-                    },
-                    Err(err) => {
-                        writeln!(stderr(), "wget: failed to read response: {}", err).unwrap();
-                        process::exit(1);
-                    }
+fn wget<W: Write>(url: &str, mut output: W) {
+    let mut client = Client::with_connector(HttpsConnector::new(hyper_rustls::TlsClient::new()));
+    client.set_read_timeout(Some(Duration::new(5, 0)));
+    client.set_write_timeout(Some(Duration::new(5, 0)));
+    match client.get(url).send() {
+        Ok(mut res) => match res.status {
+            StatusCode::Ok => match io::copy(&mut res, &mut output) {
+                Ok(_) => {
+                    let _ = output.flush();
+                },
+                Err(err) => {
+                    writeln!(stderr(), "wget: failed to transfer data: {}", err).unwrap();
+                    process::exit(1);
                 }
             },
-            Err(err) => {
-                writeln!(stderr(), "wget: failed to send request: {}", err).unwrap();
+            _ => {
+                writeln!(stderr(), "wget: failed to receive request: {}", res.status).unwrap();
                 process::exit(1);
             }
+        },
+        Err(err) => {
+            writeln!(stderr(), "wget: failed to send request: {}", err).unwrap();
+            process::exit(1);
         }
-    } else {
-        writeln!(stderr(), "wget http://host:port/path").unwrap();
-        process::exit(1);
+    }
+}
+
+fn main() {
+    let mut args = env::args().skip(1);
+    match args.next() {
+        Some(url) => match args.next() {
+            Some(path) => match File::create(&path) {
+                Ok(file) => {
+                    wget(&url, file);
+                },
+                Err(err) => {
+                    writeln!(stderr(), "wget: failed to create '{}': {}", path, err).unwrap();
+                    process::exit(1);
+                }
+            },
+            None => {
+                wget(&url, stdout());
+            }
+        },
+        None => {
+            writeln!(stderr(), "wget http://host:port/path [output]").unwrap();
+            process::exit(1);
+        }
     }
 }
